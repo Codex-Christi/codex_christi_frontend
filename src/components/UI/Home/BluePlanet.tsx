3d2aec02c00b9031642b7ca152c9e924b322b67b
Saint Tarila Brisbe <saintbrisbe@gmail.com>
2024-06-28 13:32:16 -0700

    Started home and main nav pt.2

--- src/components/UI/Home/BluePlanet.tsx
+++ src/components/UI/Home/BluePlanet.tsx
@@ -9,8 +9,8 @@ const BluePlanet = () => {
       width={0}
       src='/media/img/home/blue-planet.svg'
       className={`w-[80%] max-w-[550px] h-[8.5rem] right-[-4rem] top-[-1.25rem]
-        md:w-[65%] md:max-w-[500px] md:h-[9rem] md:right-[-5.5rem]
-        lg:w-[35%] lg:max-w-[400px] lg:h-[160px] lg:right-0
+        md:w-[65%] md:max-w-[500px] md:h-[9rem] md:right-[-4.5rem]
+        lg:w-[35%] lg:max-w-[470px] lg:h-[200px] lg:right-0 lg:top-[-.1rem]
         absolute z-[-1]  `}
     />
   );
