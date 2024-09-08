f9f15e38b790e1a7144be0b487a6b59affc6af55
Saint Tarila Brisbe <saintbrisbe@gmail.com>
2024-09-07 19:06:36 -0700

    Started nav on tablet and desktop pt 2

--- frontend/src/components/UI/general/MainNav/NavList.tsx
+++ frontend/src/components/UI/general/MainNav/NavList.tsx
@@ -1,17 +1,36 @@
-import React from 'react';
+import Link from 'next/link';
+import { BsCaretRightFill } from 'react-icons/bs';
 
-const navListArr = ['feed', 'community', 'messages', 'live', 'shop'];
+const navListArr = ['feed', 'community', 'messages', 'live', 'shop'] as const;
 
 const NavList = () => {
   return (
     <section
-      className={`hidden md:flex md:gap-2.5 lg:gap-5 !font-montserrat font-semibold`}
+      className={`hidden md:flex md:gap-3.5 lg:gap-5 text-lg !font-montserrat font-semibold`}
     >
-      {navListArr.map((linkText) => {
-        return <h1 key={linkText}>{linkText.toLocaleUpperCase()}</h1>;
+      {navListArr.map((linkText: (typeof navListArr)[number]) => {
+        if (linkText === 'live') {
+          return (
+            <Link
+              href={linkText}
+              className='flex items-center gap-1'
+              key={linkText}
+            >
+              {linkText.toLocaleUpperCase()}
+              <BsCaretRightFill />
+            </Link>
+          );
+        }
+        return (
+          <Link href={linkText} key={linkText}>
+            {linkText.toLocaleUpperCase()}
+          </Link>
+        );
       })}
     </section>
   );
 };
+
+//  CREATE ACTIVELINK COMPONENT AND PASS IN PROPS LIKE TEXTNAME, CHILDREN AND EXTEND STYLEPROPS
 
 export default NavList;
