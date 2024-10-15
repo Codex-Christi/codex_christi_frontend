aaeb89a2935d2016316927b7c3ab3e8176cbafcf
Saint <devsantorz@Mac.lan>
2024-10-15 05:48:18 -0700

    Modified on Mac, part 1

--- src/lib/hooks/authHooks/useSignUp.ts
+++ src/lib/hooks/authHooks/useSignUp.ts
@@ -1,12 +1,16 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
+import axios, { AxiosError } from 'axios';
 
 const baseUrl = 'https://saintproject.onrender.com/api/v1';
 
+// Types and Interfaces
+type UserDataSendType = { name: string; email: string; password: string };
+type UserDataReturnType = { id: Number; name: string; email: string };
 interface SignupHookInterface {
   isLoading: boolean;
   isError: boolean;
   erroMsg: string;
-  userData: { id: Number; name: string; email: string } | null;
+  userData: UserDataSendType | null;
 }
 
 const defaultSignUpProcessState: SignupHookInterface = {
@@ -18,8 +22,18 @@ const defaultSignUpProcessState: SignupHookInterface = {
 
 // Main SignUp Hook
 export const useRegularSignUp = () => {
+  // State values
   const [loginProcessState, setLoginProcessState] =
     useState<SignupHookInterface>(defaultSignUpProcessState);
 
-  return { loginProcessState };
+  const signUp = useCallback(async (userDetails: UserDataSendType) => {
+    await axios
+      .post<UserDataReturnType>(`${baseUrl}/users`, {
+        ...userDetails,
+      })
+      .then((response) => response.data)
+      .catch((err: AxiosError | any) => err.message);
+  }, []);
+
+  return { ...loginProcessState };
 };
