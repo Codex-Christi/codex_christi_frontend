4b183f798d9667b0f785387fc1f11a2b4fd40be3
Wisdom Ojimah <vindicatedwisdom@gmail.com>
2024-11-17 11:04:45 +0100

    chore(seo): add opengraph image to about page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,55 +10,58 @@ import { Toaster } from '@/components/UI/primitives/toaster';
 import FaviconUpdater from '@/components/UI/general/Helpers/FaviconUpdater';
 
 const nicoMoji = localFont({
-  src: '../res/fonts/Nico-Moji.woff',
-  variable: '--font-nico',
+	src: '../res/fonts/Nico-Moji.woff',
+	variable: '--font-nico',
 });
 const OCR_ext = localFont({
-  src: '../res/fonts/OCR-ext.ttf',
-  variable: '--font-ocr',
+	src: '../res/fonts/OCR-ext.ttf',
+	variable: '--font-ocr',
 });
 const MontserratFont = Montserrat({
-  subsets: ['latin'],
-  variable: '--font-montserrat',
+	subsets: ['latin'],
+	variable: '--font-montserrat',
 });
 
 export async function generateMetadata(): Promise<Metadata> {
-  return {
-    // dynamically get the host from the Next headers
-    metadataBase: new URL(`https://${(await headers()).get('host')}`),
-    title: 'Codex Christi',
-    description:
-      'A Hub for Christian Creatives to connect, share, and glorify God.',
-  };
+	return {
+		// dynamically get the host from the Next headers
+		metadataBase: new URL(`https://${(await headers()).get('host')}`),
+		title: 'Codex Christi',
+		description:
+			'A Hub for Christian Creatives to connect, share, and glorify God.',
+	};
 }
 
 export const viewport: Viewport = {
-  initialScale: 1,
-  viewportFit: 'cover',
-  width: 'device-width',
+	initialScale: 1,
+	viewportFit: 'cover',
+	width: 'device-width',
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  // Main HTML Document
-  return (
-    <html lang='en' className='!overflow-x-hidden !overflow-y-auto '>
-      {/* !w-screen */}
-      <body
-        className={cn(
-          ` font-montserrat bg-black text-white !max-w-full !overflow-x-hidden antialiased`,
-          nicoMoji.variable,
-          OCR_ext.variable,
-          MontserratFont.variable
-        )}
-      >
-        <FaviconUpdater />
-        <Toaster />
-        {children}
-      </body>
-    </html>
-  );
+	// Main HTML Document
+	return (
+		<html
+			lang='en'
+			className='!overflow-x-hidden !overflow-y-auto '
+		>
+			{/* !w-screen */}
+			<body
+				className={cn(
+					` font-montserrat bg-black text-white !max-w-full !overflow-x-hidden antialiased`,
+					nicoMoji.variable,
+					OCR_ext.variable,
+					MontserratFont.variable,
+				)}
+			>
+				<FaviconUpdater />
+				<Toaster />
+				{children}
+			</body>
+		</html>
+	);
 }
