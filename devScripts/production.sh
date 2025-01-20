37985f91b1eda9bfbfa42876c608d7ec9d17879a
Saint Tarila Brisbe <saintbrisbe@gmail.com>
2025-01-20 12:56:23 -0800

    Started adding server logging capablities pt. 2

--- devScripts/production.sh
+++ devScripts/production.sh
@@ -4,7 +4,7 @@ set -exo pipefail
 
 # Build and run the latest version of the app
 docker compose -f docker-compose.production.yml down
-docker compose --file docker-compose.production.yml up --build --detach nginx
+docker compose --file docker-compose.production.yml up --build --detach nginx >fe_logs.txt
 
 # Remove the unused containers
 docker system prune --force
