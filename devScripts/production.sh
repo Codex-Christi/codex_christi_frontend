abbcc9f4bf7349d8242eed22befd0b6f773f99e4
sylvas-j <sylvanusjerome@gmail.com>
2025-01-23 15:00:02 +0100

    curl

--- devScripts/production.sh
+++ devScripts/production.sh
@@ -3,7 +3,8 @@
 set -exo pipefail
 
 # Build and run the latest version of the app
-docker compose --file docker-compose.production.yml up --build --detach nginx > serverlogs.txt
+# docker compose --file docker-compose.production.yml up --build --detach nginx > serverlogs.txt
+docker compose --file docker-compose.production.yml up --build --detach
 
 # Remove the unused containers
 docker system prune --force
