b27d22766758ab4bb64370742e0e260ffc15206c
Saint Tarila Brisbe <saintbrisbe@gmail.com>
2025-01-26 11:14:05 -0800

    Started middleware implementation pt 2

--- src/middleware.ts
+++ src/middleware.ts
@@ -22,21 +22,21 @@ export function middleware(req: NextRequest) {
 }
 
 // Matcher for filtering middleware
-export const config = {
-  matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - api (API routes)
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
-     */
-    '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)',
-    // {
-    //   source:
-    //     '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)',
-    //   has: [{ type: 'header', key: 'x-present' }],
-    //   missing: [{ type: 'header', key: 'x-missing', value: 'prefetch' }],
-    // },
-  ],
-};
+// export const config = {
+//   matcher: [
+//     /*
+//      * Match all request paths except for the ones starting with:
+//      * - api (API routes)
+//      * - _next/static (static files)
+//      * - _next/image (image optimization files)
+//      * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+//      */
+//     '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)',
+//     // {
+//     //   source:
+//     //     '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)',
+//     //   has: [{ type: 'header', key: 'x-present' }],
+//     //   missing: [{ type: 'header', key: 'x-missing', value: 'prefetch' }],
+//     // },
+//   ],
+// };
