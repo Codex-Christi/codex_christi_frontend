ec7ac6cf36a723451415b4e7ce396a489c55c785
Saint Tarila Brisbe <saintbrisbe@gmail.com>
2025-01-27 02:31:20 -0800

    Added og-image for shop route

--- src/app/shop/layout.tsx
+++ src/app/shop/layout.tsx
@@ -1,4 +1,17 @@
+import { Metadata } from 'next';
+import { headers } from 'next/headers';
 import { ReactNode } from 'react';
+
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    // dynamically get the host from the Next headers
+    metadataBase: new URL(`https://${(await headers()).get('host')}`),
+    title: 'Shop for Godly, and Creative Merch | Codex Christi Shop',
+    description: `Discover Godly and creative merch at Codex Christi Shop! Explore unique, 
+    faith-inspired apparel, accessories, and gifts designed to inspire and uplift. 
+    Shop now for meaningful creations that celebrate your faith!`,
+  };
+}
 
 export default function ShopRootLayout({ children }: { children: ReactNode }) {
   return (
