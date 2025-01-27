64cb84c4c7df86275633d8de17234b7efec0f48b
Saint Tarila Brisbe <saintbrisbe@gmail.com>
2025-01-26 23:45:35 -0800

    handling image requests with and without query parameters 2

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,22 +3,12 @@ import logger from './logger';
 
 export function middleware(req: NextRequest) {
   const url = req.nextUrl.clone();
-  const hostname = req.headers.get('host'); // Get the hostname of the incoming request
+  const hostname = req.headers.get('host'); // Get the incoming hostname
 
   // Check if the hostname is 'codexchristi.shop'
   if (hostname === 'codexchristi.shop') {
     // Logging to track the middleware activity
     logger.info(`Middleware triggered for ${url.pathname} on ${hostname}`);
-
-    // Checking if the request is for an image (jpg, jpeg, png, etc.)
-    const isImageRequest = /\.(jpg|jpeg|png|gif|webp|svg)(\?|\b|$)/i.test(
-      url.pathname
-    );
-
-    // If it's an image request, skip the middleware (pass through the request)
-    if (isImageRequest) {
-      return NextResponse.next(); // Allow image requests to bypass middleware
-    }
 
     // Serve /shop and its child routes without rewriting the URL in the browser
     if (!url.pathname.startsWith('/shop')) {
@@ -31,24 +21,15 @@ export function middleware(req: NextRequest) {
   return NextResponse.next(); // Proceed with the default Next.js response
 }
 
-// Matcher for filtering middleware
-/*
- * Match all request paths except for the ones starting with:
- * - api (API routes)
- * - _next/static (static files)
- * - _next/image (image optimization files)
- * - favicon.ico, sitemap.xml, robots.txt (metadata files)
- */
+// Config for middleware matcher
 export const config = {
   matcher: [
-    // Match all paths except for specific static paths
-    '/((?!api|_next/static|_next/image|wp-admin|favicon.ico|sitemap.xml|robots.txt).*)',
-
     {
-      // Match requests that are missing specific headers (e.g., prefetch headers)
+      source:
+        '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|.*\.(jpg|jpeg|png|gif|webp|svg)).*)', // Exclude images and static files
       missing: [
-        { type: 'header', key: 'next-router-prefetch' }, // Missing 'next-router-prefetch'
-        { type: 'header', key: 'purpose', value: 'prefetch' }, // Missing 'purpose: prefetch'
+        { type: 'header', key: 'next-router-prefetch' },
+        { type: 'header', key: 'purpose', value: 'prefetch' },
       ],
     },
   ],
