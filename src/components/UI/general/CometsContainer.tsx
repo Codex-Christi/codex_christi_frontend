970c94e139554274b2e377b931260b923b58972b
Saint Tarila Brisbe <saintbrisbe@gmail.com>
2025-01-28 05:24:42 -0800

    Started putting animated bg on shop pt 2

---
+++ src/components/UI/general/CometsContainer.tsx
@@ -0,0 +1,37 @@
+import { FC, ReactNode } from 'react';
+
+interface CometsContainerInterface {
+  children?: ReactNode;
+}
+
+const CometsContainer: FC<CometsContainerInterface> = (props) => {
+  // Props
+  const { children } = props || {};
+
+  // Main JSX
+  return (
+    <div
+      className={`${children ? 'relative' : 'absolute'} top-0 bottom-0 left-0 right-0 w-full 
+      ${children ? '!-z-[1]' : '!-z-[5]'} `}
+    >
+      <div
+        className={`relative top-0 bottom-0 left-0 right-0 w-full h-full 
+          ${children ? '!-z-[1]' : '!-z-[5]'} ${children ? 'min-h-dvh' : 'min-h-[auto]'}
+          bg-[url('/media/img/home/comets_mobile.svg')]
+          md:bg-[url('/media/img/home/comets_tablet.svg')]
+          lg:bg-[url('/media/img/home/comets_ desktop.svg')]
+          bg-repeat animate-ltr-linear-infinite !ease-linear`}
+      />
+
+      {children ? (
+        <div
+          className={`relative top-0 bottom-0 left-0 right-0 w-full h-full !z-[1]`}
+        >
+          {children}
+        </div>
+      ) : null}
+    </div>
+  );
+};
+
+export default CometsContainer;
