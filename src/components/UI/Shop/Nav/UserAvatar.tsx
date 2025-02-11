50e614170faaea9e458f9cb90646ac254ea0a306
Saint Tarila Brisbe <saintbrisbe@gmail.com>
2025-02-11 05:41:58 -0800

    Finishing top right desktop nav pt. 2

---
+++ src/components/UI/Shop/Nav/UserAvatar.tsx
@@ -0,0 +1,42 @@
+import { FC, useState } from 'react';
+import Image, { ImageProps } from 'next/image';
+import { Skeleton } from '../../primitives/skeleton';
+
+type UserAvatarInterface = Omit<ImageProps, 'width' | 'height'> & {
+  width: number;
+  height: number;
+  username?: string;
+};
+
+const UserAvatar: FC<UserAvatarInterface> = (props) => {
+  // Hooks
+  const [loaded, setLoaded] = useState<boolean>(false);
+
+  // Props
+  const { width, height, username } = props;
+
+  return (
+    <div
+      className={`flex flex-col `}
+      // min-h-[${height + 12}px] min-w-[${width + 12}px]
+    >
+      {!loaded && (
+        <Skeleton
+          className={`h-[${height ? height : 25}px] w-[${width ? width : 25}px] rounded-full p-0`}
+        />
+      )}
+      <Image
+        {...props}
+        alt='User Avatar'
+        className='rounded-full'
+        onLoad={() => setLoaded(true)}
+      />
+
+      <small className='text-xs font-semibold leading-none mt-2'>
+        {username ? username : 'User'}
+      </small>
+    </div>
+  );
+};
+
+export default UserAvatar;
