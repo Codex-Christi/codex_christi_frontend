fbd57a112498b3ff2c6923dd46435f7027ffd789
Saint Tarila Brisbe <saintbrisbe@gmail.com>
2025-03-24 12:35:37 -0700

    Created fake TopHomeBanner pt. 2

---
+++ src/components/UI/Shop/ShopHomepageComponents/TopHomeBanner.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Image from 'next/image';
+
+const TopHomeBanner = () => {
+  return (
+    <div className='w-full max-w-full relative !overflow-x-hidden'>
+      <Image
+        width={1500}
+        height={100}
+        className={`!max-w-[107vw] lg:!w-[120vw]`}
+        src='/media/img/shop/ShopHome/TopHomeBanner.svg'
+        alt='Top Home Banner'
+      />
+    </div>
+  );
+};
+
+export default TopHomeBanner;
