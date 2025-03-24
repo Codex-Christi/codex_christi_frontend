84dbb01dd28722c65bdf18db1c70c8a2d482d3cf
Saint Tarila Brisbe <saintbrisbe@gmail.com>
2025-03-24 12:35:29 -0700

    Created fake TopHomeBanner pt. 1

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,7 +1,27 @@
+'use client';
+import { Button } from '@/components/UI/primitives/button';
 import { FC } from 'react';
+import axios from 'axios';
+import TopHomeBanner from '@/components/UI/Shop/ShopHomepageComponents/TopHomeBanner';
 
 const Shop: FC = () => {
-  return <div className='relative'>Shop Home Page</div>;
+  const logOut = async () => {
+    await axios
+      .get(`${process.env.NEXT_PUBLIC_BASE_URL}/v1/signout`)
+      .then((resp) => {
+        console.log('Logout successful');
+      });
+  };
+
+  // Main Shop Home Page Client JSX
+  return (
+    <div className='relative flex flex-col'>
+      <TopHomeBanner />
+      <Button name='Logout button' className='w-20' onClick={logOut}>
+        Logout
+      </Button>
+    </div>
+  );
 };
 
 export default Shop;
