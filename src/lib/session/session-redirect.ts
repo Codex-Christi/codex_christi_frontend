861ba029e0616aca5cf33029176022f1264915a7
Saint Tarila Brisbe <saintbrisbe@gmail.com>
2025-04-23 11:45:45 -0700

    Started jose cookie management 4 (Added client -> cookie communication) pt 7 (Started user redirection successfully

--- src/lib/session/session-redirect.ts
+++ src/lib/session/session-redirect.ts
@@ -1,12 +1,15 @@
-import { redirect } from 'next/navigation';
 import { verifySession } from './session-validate';
+import { NextRequest, NextResponse } from 'next/server';
 
-export const basicRedirect = (destination: string) => {
-  redirect(destination.startsWith('/') ? destination : '/');
+export const basicRedirect = (destination: string, req: NextRequest) => {
+  return NextResponse.redirect(
+    new URL(destination.startsWith('/') ? destination : '/', req.url)
+  );
 };
 
-export const redirectLoggedInUserToProfile = async () => {
+export const redirectLoggedInUserToProfile = async (req: NextRequest) => {
+  // If user is logged in already, sedn them to profile page and show alert about logged in status
   if ((await verifySession()) === true) {
-    console.log('/profile');
+    return basicRedirect('/profile', req);
   }
 };
