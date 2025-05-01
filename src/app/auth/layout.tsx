3c0fc69858f12939056219ebacadf24f8c15752a
Wisdom Ojimah <vindicatedwisdom@gmail.com>
2025-05-01 10:13:39 +0100

    fix: fix auth layout issues

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,9 +1,13 @@
-import DefaultPageWrapper from '@/components/UI/general/DefaultPageWrapper';
+import DefaultPageWrapper from "@/components/UI/general/DefaultPageWrapper";
 import FormContainer from "@/components/UI/Auth/FormContainer";
-import { FC, ReactNode } from 'react';
+import { FC, ReactNode } from "react";
 
 const SignupLayout: FC<{ children: ReactNode }> = ({ children }) => {
-  return <DefaultPageWrapper><FormContainer>{children}</FormContainer></DefaultPageWrapper>;
+	return (
+		<DefaultPageWrapper>
+			<FormContainer>{children}</FormContainer>
+		</DefaultPageWrapper>
+	);
 };
 
 export default SignupLayout;
