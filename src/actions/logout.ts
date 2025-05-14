7d780b90aa31ffc1b6936e15a634f5f5cc931635
Wisdom Ojimah <vindicatedwisdom@gmail.com>
2025-05-14 11:05:57 +0100

    fix: fix edit profile modal fields misalignment

--- src/actions/logout.ts
+++ src/actions/logout.ts
@@ -1,36 +1,41 @@
-'use server';
+"use server";
 
-import { decrypt, deleteSession } from '@/lib/session/main-session';
-import axios from 'axios';
-import { getCookie } from '@/lib/session/main-session';
-import { clearUserMainProfileStore } from '@/stores/userMainProfileStore';
+import { decrypt, deleteSession } from "@/lib/session/main-session";
+import axios from "axios";
+import { getCookie } from "@/lib/session/main-session";
+import { clearUserMainProfileStore } from "@/stores/userMainProfileStore";
 
 const axiosClient = axios.create({
-  baseURL: `${process.env.NEXT_PUBLIC_BASE_URL}`,
+	baseURL: `${process.env.NEXT_PUBLIC_BASE_URL}`,
 });
 
 export const logoutUser = async () => {
-  try {
-    const sessionCookie = await getCookie('session');
+	try {
+		const sessionCookie = await getCookie("session");
 
-    const decryptedSessionCookie = await decrypt(sessionCookie?.value);
-    const mainAccessToken = decryptedSessionCookie
-      ? (decryptedSessionCookie.mainAccessToken as string)
-      : ('' as string);
-    await axiosClient.post('/auth/user-logout', {
-      headers: {
-        Authorization: `Bearer ${mainAccessToken}`,
-      },
-    });
-    await deleteSession();
-    clearUserMainProfileStore();
-    return true;
-  } catch (err: Error | unknown) {
-    await deleteSession();
-    console.log(`Failed to logout user. Reason: ${err}`);
-    return {
-      status: false,
-      error: `${err}`,
-    };
-  }
+		const decryptedSessionCookie = await decrypt(sessionCookie?.value);
+
+		const mainAccessToken = decryptedSessionCookie
+			? (decryptedSessionCookie.mainAccessToken as string)
+			: ("" as string);
+
+		await axiosClient.post("/auth/user-logout", {
+			headers: {
+				Authorization: `Bearer ${mainAccessToken}`,
+			},
+        });
+
+        await deleteSession();
+
+        clearUserMainProfileStore();
+
+		return true;
+	} catch (err: Error | unknown) {
+		await deleteSession();
+		console.log(`Failed to logout user. Reason: ${err}`);
+		return {
+			status: false,
+			error: `${err}`,
+		};
+	}
 };
