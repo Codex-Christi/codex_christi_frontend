3d75745f0fe216f9076b5a5d4e6bb791a7d28c2a
Wisdom Ojimah <vindicatedwisdom@gmail.com>
2025-05-26 14:01:08 +0100

    fix: fix loggin status check

--- src/components/UI/Shop/Nav/NavTopRightLinks.tsx
+++ src/components/UI/Shop/Nav/NavTopRightLinks.tsx
@@ -1,49 +1,59 @@
-import { FC } from 'react';
-import { SearchButtonOnly } from './NavSearch';
-import { CartIcon } from './NavIcons';
-import { Heart } from 'lucide-react';
-import CustomShopLink from '../HelperComponents/CustomShopLink';
-import UserAvatar from './UserAvatar';
+import { FC } from "react";
+import { SearchButtonOnly } from "./NavSearch";
+import { CartIcon } from "./NavIcons";
+import { Heart } from "lucide-react";
+import CustomShopLink from "../HelperComponents/CustomShopLink";
+import UserAvatar from "./UserAvatar";
 
 const NavTopRightLinks: FC = () => {
-  // Hooks
+	// Hooks
 
-  // JSX
-  return (
-    <section
-      className={`flex items-center justify-between 
-    gap-6 
+	// JSX
+	return (
+		<section
+			className={`flex items-center justify-between
+    gap-6
     sm:gap-8
     md:gap-10
     lg:gap-12`}
-    >
-      <SearchButtonOnly
-        name='Search Button'
-        className='scale-125 relative block lg:!hidden'
-        isDesktopOnly={false}
-      />
+		>
+			<SearchButtonOnly
+				name="Search Button"
+				className="scale-125 relative block lg:!hidden"
+				isDesktopOnly={false}
+			/>
 
-      <>
-        {['cart', 'favorites', 'profile'].map((str, index) => {
-          const href = '/shop/' + str;
-          return (
-            <CustomShopLink href={href} key={str + index}>
-              {str === 'cart' && <CartIcon />}
-              {str === 'favorites' && <Heart />}
-              {str === 'profile' && (
-                <UserAvatar
-                  src='https://avatar.iran.liara.run/public'
-                  alt={'User avatar'}
-                  width={25}
-                  height={25}
-                />
-              )}
-            </CustomShopLink>
-          );
-        })}
-      </>
-    </section>
-  );
+			<>
+				{["cart", "favorites", "profile"].map((str, index) => {
+					let href = "";
+
+					if (str === "profile") {
+						href = "/" + str;
+					} else {
+						href = "/shop/" + str;
+					}
+
+					return (
+						<CustomShopLink
+							href={href}
+							key={str + index}
+						>
+							{str === "cart" && <CartIcon />}
+							{str === "favorites" && <Heart />}
+							{str === "profile" && (
+								<UserAvatar
+									src="https://avatar.iran.liara.run/public"
+									alt={"User avatar"}
+									width={25}
+									height={25}
+								/>
+							)}
+						</CustomShopLink>
+					);
+				})}
+			</>
+		</section>
+	);
 };
 
 export default NavTopRightLinks;
--- src/lib/middlewares/auth-middleware.ts
+++ src/lib/middlewares/auth-middleware.ts
@@ -10,12 +10,19 @@ export const redirectLoggedInUserToProfileMiddleware = async (
 };
 
 export const redirectExpSessionToLoginPage = async (req: NextRequest) => {
-  const isSessionAvailable = (await getCookie('session')) ? true : false;
+    try {
+		const isSessionAvailable = (await getCookie("session")) ? true : false;
 
-  if (!isSessionAvailable) {
-    await logoutUser();
-    return NextResponse.redirect(
-      new URL('/auth/sign-in?sessionExp=true', req.url)
-    );
-  }
+		if (!isSessionAvailable) {
+			await logoutUser();
+
+			return NextResponse.redirect(
+				new URL("/auth/sign-in?sessionExp=true", req.url),
+			);
+		}
+	} catch {
+        return NextResponse.redirect(
+			new URL("/auth/sign-in?sessionExp=true", req.url),
+		);
+	}
 };
