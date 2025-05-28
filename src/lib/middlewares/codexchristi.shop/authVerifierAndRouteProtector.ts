25d1c7ad8c6f9da86e484a8fb208a0a27a6dcb41
Saint Tarila Brisbe <saintbrisbe@gmail.com>
2025-05-28 09:03:54 -0700

    Started auth middleware on codexchristi.shop domain pt. 11

--- src/lib/middlewares/codexchristi.shop/authVerifierAndRouteProtector.ts
+++ src/lib/middlewares/codexchristi.shop/authVerifierAndRouteProtector.ts
@@ -6,8 +6,10 @@ export const authVerifierAndRouteProtector = async (req: NextRequest) => {
   const hostname = req.headers.get('host'); // Get the incoming hostname
   if (hostname === 'codexchristi.shop') {
     try {
-      const referrer = req.headers.get('referer')?.split(hostname)[1];
-      const url = req.nextUrl.clone().toString()?.split(hostname)[1];
+      const referrer = req.headers.get('referer')!.split(hostname)[1];
+      const url = req.nextUrl.clone().toString()!.split(hostname)[1];
+
+      console.log(`Referrer: ${referrer}, Current URL: ${url}`);
 
       if (referrer && referrer === url.toString()) {
         if ((await verifySession()) === true && req.method === 'GET') {
