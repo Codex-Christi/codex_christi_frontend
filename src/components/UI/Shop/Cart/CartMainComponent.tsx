259ff3b93dfce8ad475a16124b3ff08a694a45b1
Saint Tarila Brisbe <saintbrisbe@gmail.com>
2025-06-03 12:36:45 -0700

    Started rendering live product data pt. 22 - Adding cart main page pt 4

--- src/components/UI/Shop/Cart/CartMainComponent.tsx
+++ src/components/UI/Shop/Cart/CartMainComponent.tsx
@@ -4,6 +4,10 @@ import { useCartStore } from '@/stores/shop_stores/cartStore';
 import { useEffect, useMemo } from 'react';
 import dynamic from 'next/dynamic';
 import Image from 'next/image';
+import {
+  hasColorAndSize,
+  ProductVariantOptions,
+} from '@/app/shop/product/[id]/productDetailsSSR';
 
 const CartEmptyComponent = dynamic(
   () => import('./CartEmptyComponent').then((mod) => mod.CartEmptyComponent),
@@ -29,22 +33,33 @@ const CartMainComponent = () => {
   return (
     <div className='px-2 py-12 md:px-[20px] lg:px-[24px]'>
       <h2 className='font-extrabold text-white text-4xl font-ocr'>Your Cart</h2>
-      <div className='bg-[#3D3D3D4D] text-white rounded-[20px] grid place-content-center gap-4 min-h-[60svh] w-full py-12'>
+      <div className='bg-[#3D3D3D4D] backdrop-blur-[5px] text-white rounded-[20px] grid place-content-center gap-4 min-h-[60svh] w-full py-12'>
         {isCartEmpty && <CartEmptyComponent />}
 
         {/* If the cart is not empty, render the cart items */}
         {!isCartEmpty && variants && (
-          <div className='grid gap-8'>
-            {variants.map(({ itemDetail: variant, title }) => {
+          <div
+            className='flex flex-col gap-8 max-h-[90vh] px-5 !w-full 
+          md:!w-[90vw] lg:!w-[85vw] maz-w-[1200px] overflow-y-auto scrollbar'
+          >
+            {variants.map(({ itemDetail: variant, title, quantity }) => {
               const productTitle = title;
-              const { title: variantTitle, image, retail_price, _id } = variant;
+              const {
+                title: variantTitle,
+                image,
+                retail_price,
+                _id,
+                options,
+              } = variant;
+              const isSizeAndColor = hasColorAndSize(
+                options as ProductVariantOptions
+              );
 
               return (
                 <div
                   key={_id}
                   className='bg-gray-700 bg-opacity-80 p-4 rounded-lg shadow-sm 
-                   shadow-gray-200 w-full md:w-[600px] flex lg:w-[800px] mx-auto
-                   justify-between'
+                   shadow-gray-200 w-full flex mx-auto justify-between'
                 >
                   {/* Render variant details here */}
                   {image && (
@@ -56,9 +71,26 @@ const CartMainComponent = () => {
                       alt={title}
                     />
                   )}
-                  <section>
-                    <h3 className='font-bold text-xl'>{productTitle}</h3>
-                    <p>{`Price: $${retail_price}`}</p>
+
+                  {/* Title , price and quantity */}
+                  <section className='font-semibold text-left h-full flex flex-col justify-between'>
+                    <h3 className='text-xl'>{productTitle}</h3>
+                    <h4>{`Price: $${retail_price} (each)`}</h4>
+                    <h4>{`Quantity: ${quantity}`}</h4>
+                  </section>
+
+                  {/* Size and Color */}
+                  <section
+                    className='font-semibold !text-left h-full flex flex-col justify-center 
+                  !items-start'
+                  >
+                    {isSizeAndColor && <h4>Color:{options[2]?.name}</h4>}
+                    <h4>
+                      Size:{' '}
+                      {isSizeAndColor
+                        ? options[1]?.value.toUpperCase()
+                        : options[0].value.toUpperCase()}
+                    </h4>
                   </section>
                 </div>
               );
