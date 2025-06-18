a3c39ef09249216f51a81c7839809cc057b51477
Saint Tarila Brisbe <saintbrisbe@gmail.com>
2025-06-18 11:54:41 -0700

    Integrating Paypal pt. 6

---
+++ src/components/UI/Shop/Checkout/OrderSummary.tsx
@@ -0,0 +1,74 @@
+import Image from 'next/image';
+import ProductImage from '@/assets/img/t-shirt-2.png';
+
+const OrderSummary = () => {
+  return (
+    <div className='bg-[#4C3D3D3D] backdrop-blur-[10px] p-4 rounded-[10px] md:p-10 space-y-8 lg:col-span-5'>
+      <h2 className='border-b border-white pb-1 text-xl font-bold'>
+        Order Summary
+      </h2>
+
+      <div className='grid gap-12'>
+        <div className='flex items-start justify-between gap-4'>
+          <div className='flex items-center gap-4'>
+            <Image
+              className='rounded-[10px] w-10 h-12 object-cover object-center'
+              src={ProductImage}
+              alt='Ton blue hoodie '
+            />
+
+            <div className='text-[#F3F3F3] text-sm space-y-0.5'>
+              <p className='font-semibold'>Ton blue hoodie</p>
+
+              <p className='text-[#F3F3F3]/70'>Size: M</p>
+
+              <p className='text-[#F3F3F3]/70'>Colour: Ton Blue</p>
+            </div>
+          </div>
+
+          <div className='text-[#F3F3F3] space-y-0.5'>
+            <p className='font-semibold'>N34,700</p>
+
+            <p className='text-right text-[#F3F3F3]/70'>Qty: 3</p>
+          </div>
+        </div>
+
+        <div className='flex items-start gap-3'>
+          <input
+            className='input w-full !px-4 !py-2.5 !rounded-[10px] !border-[#F3F3F399]'
+            type='text'
+            placeholder='Coupon or discount code'
+            id='coupon'
+            name='coupon'
+          />
+
+          <button className='btn border-[#F3F3F366] text-[#F3F3F3] bg-[#F3F3F366] hover:bg-[#F3F3F366]/30 hover:text-[#F3F3F3] !font-bold px-4 sm:px-6 !py-2.5 rounded-[10px] text-sm sm:text-base text-center shrink-0'>
+            Apply
+          </button>
+        </div>
+
+        <div className='space-y-2'>
+          <p className='flex items-center justify-between gap-4 font-semibold'>
+            <span>Subtotal</span>
+
+            <span>N34,700</span>
+          </p>
+
+          <p className='flex items-center justify-between gap-4 font-semibold'>
+            <span>Shipping/Delivery Fee</span>
+
+            <span>N3,000</span>
+          </p>
+        </div>
+
+        <p className='flex items-center justify-between gap-4 font-semibold'>
+          <span>Total</span>
+
+          <span className='text-lg'>N37,700</span>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default OrderSummary;
---
+++ src/components/UI/Shop/Checkout/ProductCheckout.tsx
@@ -0,0 +1,110 @@
+'use client';
+
+import Image from 'next/image';
+import PayPal from '@/assets/img/paypal.png';
+import GooglePay from '@/assets/img/gpay.png';
+import Link from 'next/link';
+import { Checkbox } from '@/components/UI/primitives/checkbox';
+import { useState } from 'react';
+import { Button } from '@/components/UI/primitives/button';
+import dynamic from 'next/dynamic';
+
+export type CheckoutOptions = 'card' | 'paypal_buttons' | 'google_pay' | '';
+
+// Dynamic Imports
+const OrderSummary = dynamic(() =>
+  import('./OrderSummary').then((comp) => comp.default)
+);
+const PayPalCheckout = dynamic(() =>
+  import('./Paypal/PaypalMainComponent').then((comp) => comp.default)
+);
+
+// Main Component
+const CheckoutPage = () => {
+  const [payOption, setPayOption] = useState<CheckoutOptions>('');
+
+  return (
+    <div className='grid gap-8 items-start px-2 py-12 md:px-[20px] lg:px-[24px] md:grid-cols-2 lg:grid-cols-12'>
+      <div className='bg-[#4C3D3D3D] backdrop-blur-[10px] p-4 rounded-[10px] md:p-10 space-y-8 lg:col-span-7'>
+        <h2 className='border-b border-white pb-1 text-xl font-bold'>
+          Payment Method
+        </h2>
+
+        <div className='grid gap-8'>
+          <div className='space-y-4'>
+            <p className='text-lg font-bold'>Pay with:</p>
+
+            <div className='flex items-center gap-8 flex-wrap'>
+              {/*  */}
+              {/* Checkbox for Debit / Credit Card */}
+              <p className='flex items-center gap-2'>
+                <Checkbox
+                  className={`w-[1.15rem] h-[1.15rem] border-white text-white`}
+                  checked={payOption === 'card'}
+                  onCheckedChange={() => setPayOption('card')}
+                />
+                Credit/Debit Card
+              </p>
+              {/*  */}
+
+              {/* Checkbox for Paypal Buttons */}
+              <p className='flex items-center gap-3'>
+                <Checkbox
+                  className={`w-[1.15rem] h-[1.15rem] border-white text-white`}
+                  checked={payOption === 'paypal_buttons'}
+                  onCheckedChange={() => setPayOption('paypal_buttons')}
+                />
+
+                <Image
+                  className='w-10 h-auto'
+                  src={PayPal}
+                  alt='Pay with Paypal'
+                />
+              </p>
+              {/*  */}
+
+              {/* Checkbox for Google Pay */}
+              <p className='flex items-center gap-3'>
+                <Checkbox
+                  className={`w-[1.15rem] h-[1.15rem] border-white text-white`}
+                  checked={payOption === 'google_pay'}
+                  onCheckedChange={() => setPayOption('google_pay')}
+                />
+
+                <Image
+                  className='w-10 h-auto'
+                  src={GooglePay}
+                  alt='Pay with Google Pay'
+                />
+              </p>
+              {/*  */}
+            </div>
+          </div>
+
+          <p>
+            Create your account Or{' '}
+            <span className='font-medium'>
+              Already have an account?{' '}
+              <Link
+                className='underline underline-offset-4 font-semibold'
+                href=''
+              >
+                Sign In
+              </Link>
+            </span>
+          </p>
+
+          {/* Paypal Component Here */}
+
+          <PayPalCheckout mode={payOption} />
+
+          {/*  */}
+        </div>
+      </div>
+
+      <OrderSummary />
+    </div>
+  );
+};
+
+export default CheckoutPage;
