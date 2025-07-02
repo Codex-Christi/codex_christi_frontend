53b715c13e6a8758ef73f69d7775f4af3b628ae6
Saint Tarila Brisbe <saintbrisbe@gmail.com>
2025-07-02 02:37:31 -0700

    Expanding context for Paypal payment obj pt. 4 - Added img-proxy for valid image URL format for paypal pt 9

--- src/app/api/img-proxy/[...slug]/route.ts
+++ src/app/api/img-proxy/[...slug]/route.ts
@@ -4,9 +4,9 @@ export const runtime = 'edge';
 
 export async function GET(
   req: Request,
-  { params }: { params: { slug: Promise<string[]> } }
+  { params }: { params: Promise<{ slug: string[] }> }
 ) {
-  const slug = await params.slug;
+  const { slug } = await params;
   if (!slug || slug.length < 2) {
     return new Response('Invalid proxy URL structure', { status: 400 });
   }
