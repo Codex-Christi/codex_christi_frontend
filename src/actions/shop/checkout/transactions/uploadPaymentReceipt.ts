b8dcffa1d17c1c3970774c61347a86e0f81ee500
Saint Tarila Brisbe <saintbrisbe@gmail.com>
2025-07-17 11:29:39 -0700

    Running PDF generation, pt. 5 - Adding r2 - pt.4

---
+++ src/actions/shop/checkout/transactions/uploadPaymentReceipt.ts
@@ -0,0 +1,34 @@
+import chalk from 'chalk';
+import { PutObjectCommand } from '@aws-sdk/client-s3';
+import { r2 } from '@/lib/cloudflare/r2';
+
+const objStorageDomain = `https://payment-receipts.codexchristi.shop`;
+
+export const uploadPaymentReceiptToR2 = async ({
+  fileBody,
+  filename,
+}: {
+  filename: string;
+  fileBody: Buffer<ArrayBufferLike>;
+}) => {
+  try {
+    console.log(chalk.yellow(`Generating an upload URL!`));
+
+    await r2.send(
+      new PutObjectCommand({
+        Bucket: process.env.R2_PAYMENT_DOCS_BUCKET_NAME,
+        Key: `paypal/${filename}`,
+        Body: fileBody,
+      }),
+    );
+
+    // Return Link for object
+    return { accessLink: `${objStorageDomain}/paypal/${encodeURI(filename)}` };
+
+    // Catch any error
+  } catch (err) {
+    throw new Error(
+      typeof err === 'string' ? err : err instanceof Error ? err.message : JSON.stringify(err),
+    );
+  }
+};
