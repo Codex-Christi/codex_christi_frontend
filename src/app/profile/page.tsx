0470d847bd9c6b80f8026fd21385b0421b8f0466
Saint Tarila Brisbe <saintbrisbe@gmail.com>
2025-07-25 18:16:55 -0700

    Moving entire SSR profile fetcher to profile store

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,7 +1,5 @@
 import ProfilePageMainComponent from '@/components/UI/profile/ProfilePageMainComponent';
-import { UserProfileDataInterface } from '@/lib/types/user-profile/main-user-profile';
 import { Metadata } from 'next';
-import { getUser } from '@/lib/funcs/userProfileFetchers/getUser';
 
 export const metadata: Metadata = {
   title: 'Profile | Codex Christi',
@@ -12,14 +10,5 @@ export const metadata: Metadata = {
 
 // Async SSR profile page data genarator function
 export default async function Page() {
-  const userDataApiResponse = await getUser();
-
-  const doesResponseHaveUserData = userDataApiResponse
-    ? 'first_name' in userDataApiResponse
-    : false;
-
-  if (doesResponseHaveUserData) {
-    const responseObj = userDataApiResponse as UserProfileDataInterface;
-    return <ProfilePageMainComponent serverFetchedProfileUserData={responseObj} />;
-  }
+  return <ProfilePageMainComponent />;
 }
--- src/components/UI/profile/ProfilePageMainComponent.tsx
+++ src/components/UI/profile/ProfilePageMainComponent.tsx
@@ -2,31 +2,15 @@
 import ContentContainer from '@/components/UI/profile/ContentContainer';
 import ProfileBanner from '@/components/UI/profile/ProfileBanner';
 import ProfileContainer from '@/components/UI/profile/ProfileContainer';
-import { useEffect, useState } from 'react';
 import EditProfileModal from './Edit/EditProfileModal';
-import { UserProfileDataInterface } from '@/lib/types/user-profile/main-user-profile';
-import { useUserMainProfileStore } from '@/stores/userMainProfileStore';
+import { useState } from 'react';
 
 // Interfaces
-interface ProfilePageMainComponentProps {
-  serverFetchedProfileUserData: UserProfileDataInterface;
-}
 
 // main component
-const ProfilePageMainComponent: React.FC<ProfilePageMainComponentProps> = ({
-  serverFetchedProfileUserData,
-}) => {
+const ProfilePageMainComponent: React.FC = ({}) => {
   // Hooks
   const [isActive, setIsActive] = useState(false);
-  const setUserMainProfile = useUserMainProfileStore(
-    (state) => state.setUserMainProfile
-  );
-
-  // useEffects
-  useEffect(() => {
-    // Set the user profile data in the store
-    setUserMainProfile(serverFetchedProfileUserData);
-  }, [serverFetchedProfileUserData, setUserMainProfile]);
 
   //   Main JSX
   return (
