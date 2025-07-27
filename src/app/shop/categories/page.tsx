fe67078e6ab4fceb049afc1b567490eff949e214
Saint Tarila Brisbe <saintbrisbe@gmail.com>
2025-07-26 20:57:39 -0700

    Started categories page

--- src/app/shop/categories/page.tsx
+++ src/app/shop/categories/page.tsx
@@ -12,7 +12,13 @@ const AllCategoriesPage = () => {
   );
 
   return (
-    <div className='bg-[#4C3D3D3D] backdrop-blur-[10px] py-10 my-10 rounded-[10px] w-[80vw] mx-auto max-w-[1200px]'>
+    <div
+      className=' backdrop-blur-[10px] py-10 my-10 rounded-[10px] w-[80vw] mx-auto max-w-[1200px]'
+      style={{
+        background:
+          'linear-gradient(180deg, rgba(243, 243, 243, 0.1) 0% 0%, rgba(141, 141, 141, 0.1) 100% 100%)',
+      }}
+    >
       <h1 className='text-3xl font-semibold font-inter'>Categories</h1>
       {JSON.stringify(existingCategoriesWithIconsandHrefs)}
     </div>
