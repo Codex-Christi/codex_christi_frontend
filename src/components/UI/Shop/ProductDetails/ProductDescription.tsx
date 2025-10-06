450c275310df9b86566e6904825a7247711e6ff6
Saint Tarila Brisbe <saintbrisbe@gmail.com>
2025-10-06 02:51:05 -0700

    Fix CLS on mobile for product/[id] page

--- src/components/UI/Shop/ProductDetails/ProductDescription.tsx
+++ src/components/UI/Shop/ProductDetails/ProductDescription.tsx
@@ -9,13 +9,13 @@ export const ProductDescription = () => {
 
   // Main JSX
   return (
-    <div className='bg-[#4C3D3D3D] backdrop-blur-[10px] p-4 rounded-[20px] space-y-2 lg:p-8'>
+    <div
+      className={`bg-[#4C3D3D3D] backdrop-blur-[10px] p-4 rounded-[20px] space-y-2 lg:p-8 
+    ${description.length > 300 ? 'min-h-[800px]' : ''}`}
+    >
       <h2 className='font-bold text-2xl'>Product Details</h2>
 
-      <div
-        className='grid gap-3'
-        dangerouslySetInnerHTML={{ __html: sanitizedHTML }}
-      ></div>
+      <div className='grid gap-3' dangerouslySetInnerHTML={{ __html: sanitizedHTML }}></div>
     </div>
   );
 };
