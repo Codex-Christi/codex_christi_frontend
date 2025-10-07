f07b865df2b297c7dc3e8e7186980c4b2f6b2cf5
Saint Tarila Brisbe <saintbrisbe@gmail.com>
2025-10-07 11:18:56 -0700

    SEO product page fixes pt. 18

--- src/components/UI/Shop/ProductDetails/ProductImageGallery/MainStage.tsx
+++ src/components/UI/Shop/ProductDetails/ProductImageGallery/MainStage.tsx
@@ -48,7 +48,7 @@ function MainStage({
                   src={loader.srcWithRetry(src, i)}
                   alt={metaTitle || 'Product image'}
                   // Patched sizes: cap max width to 640px
-                  sizes='(max-width: 412px) 360px, (max-width: 640px) 480px, (min-resolution: 2dppx) 50vw, 640px'
+                  sizes='(max-width: 412px) 360px, (max-width: 640px) 480px, (min-resolution: 2dppx) 45vw, (min-width: 1280px) 1000px, 640px'
                   quality={80}
                   onLoad={() => loader.markLoaded(i, src)}
                   onError={() => loader.markFailed(i)}
