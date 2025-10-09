5e51488132d0a9539886abcc6232c3f520d19789
Saint Tarila Brisbe <saintbrisbe@gmail.com>
2025-10-09 02:05:37 -0700

    SEO product page fixes pt. 22

--- src/components/UI/Shop/ProductDetails/ProductDescription.tsx
+++ src/components/UI/Shop/ProductDetails/ProductDescription.tsx
@@ -11,7 +11,7 @@ export const ProductDescription = () => {
   return (
     <div
       className={`bg-[#4C3D3D3D] backdrop-blur-[10px] p-4 rounded-[20px] space-y-2 lg:p-8 
-    ${description.length > 300 ? 'min-h-[1000px]' : ''}
+    ${description.length > 300 ? 'min-h-[1100px] sm-min-h-[950px] lg:min-h-[970px] xl:min-h-[950px]' : ''}
     `}
     >
       <h2 className='font-bold text-2xl'>Product Details</h2>
