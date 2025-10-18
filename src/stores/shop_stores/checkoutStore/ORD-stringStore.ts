fbd5efa67ddc320fb11c53490d2f1bc71d552732
Saint Tarila Brisbe <saintbrisbe@gmail.com>
2025-10-18 00:14:55 -0700

    Started ORD_String context - created ORD-string store pt. 2

---
+++ src/stores/shop_stores/checkoutStore/ORD-stringStore.ts
@@ -0,0 +1,23 @@
+import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
+import { encrypt, decrypt } from '../cartStore';
+import { createEncryptedStorage } from '.';
+
+interface OrderStringState {
+  orderString: string;
+  setOrderString: (newString: string) => void;
+}
+
+export const useOrderStringStore = create<OrderStringState>()(
+  persist(
+    (set) => ({
+      orderString: '',
+      setOrderString: (newString) => set({ orderString: newString }),
+    }),
+    {
+      name: 'order-string-store',
+      storage: createEncryptedStorage<OrderStringState>({ encrypt, decrypt }),
+      skipHydration: true,
+    },
+  ),
+);
