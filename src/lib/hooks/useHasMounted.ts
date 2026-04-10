1768623b6994e418a5465dced95640d9f2c94f16
Saint Tarila Brisbe <saintbrisbe@gmail.com>
2026-04-09 21:08:42 -0700

    Started intent route pt. 2 -fixing CI isuues pt 2

---
+++ src/lib/hooks/useHasMounted.ts
@@ -0,0 +1,9 @@
+'use client';
+
+import { useSyncExternalStore } from 'react';
+
+const subscribe = () => () => {};
+
+export function useHasMounted() {
+  return useSyncExternalStore(subscribe, () => true, () => false);
+}
