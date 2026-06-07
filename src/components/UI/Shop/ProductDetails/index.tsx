cb7b81969a3cadbe7c6855f4a587064a358f853f
Saint Tarila Brisbe <saintbrisbe@gmail.com>
2026-06-07 15:01:15 -0700

    Use one product detail responsive layout

--- src/components/UI/Shop/ProductDetails/index.tsx
+++ src/components/UI/Shop/ProductDetails/index.tsx
@@ -2,9 +2,7 @@
 import { ProductResult } from '@/lib/merchizeStorefront/productTypes';
 import dynamic from 'next/dynamic';
 const ProductSummary = dynamic(() => import('./ProductSummary'));
-const ProductTitleAndSizesEtc = dynamic(() => import('./ProductTitleAndSizesEtc'));
 import { createContext, useContext, FC, ReactNode, useMemo } from 'react';
-import { useResponsiveSSRValue } from '@/lib/hooks/useResponsiveSSR_Store';
 
 export interface ProductDetailsProps {
   // Define any props if needed
@@ -29,16 +27,12 @@ export const useProductDetailsContext = () => {
 };
 
 const ProductDetails: FC<ProductDetailsProps> = ({ fetchedProductData, descriptionSection }) => {
-  // Hooks
-  const { isDesktopOnly } = useResponsiveSSRValue();
-
   //   JSX
   return (
     // Main JSX
     <ProductDetailsContext.Provider value={useMemo(() => fetchedProductData, [fetchedProductData])}>
       <div className='grid gap-8 items-start px-2 py-12 md:px-[20px] lg:px-[24px] lg:grid-cols-6 xl:grid-cols-3'>
         <ProductSummary descriptionSection={descriptionSection} />
-        {isDesktopOnly && <ProductTitleAndSizesEtc />}
       </div>
     </ProductDetailsContext.Provider>
   );
