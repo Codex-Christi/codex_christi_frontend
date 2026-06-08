32e20efc855e716595480e530971573238ca82ee
Saint Tarila Brisbe <saintbrisbe@gmail.com>
2026-06-08 01:09:58 -0700

    Remove legacy category query redirect

--- next.config.mjs
+++ next.config.mjs
@@ -49,22 +49,6 @@ const nextConfig = {
       },
     ];
   },
-  async redirects() {
-    return [
-      {
-        source: '/shop/category/:id',
-        has: [
-          {
-            type: 'query',
-            key: 'page',
-            value: '(?<page>\\d+)',
-          },
-        ],
-        destination: '/shop/category/:id/page/:page',
-        permanent: false,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
