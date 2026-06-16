3af2dbee584358514444de584ca0d078f9094a26
Saint Tarila Brisbe <saintbrisbe@gmail.com>
2026-06-16 05:02:57 -0700

    Serve shop logo in modern image formats

--- src/components/UI/Shop/Logo.tsx
+++ src/components/UI/Shop/Logo.tsx
@@ -11,21 +11,27 @@ export default function Logo() {
       href='/'
       className='inline-block'
     >
-      {/* eslint-disable-next-line @next/next/no-img-element */}
-      <img
-        width={72}
-        height={48}
-        className='relative block pointer-events-auto !max-w-[unset]'
-        style={{ width: '72px', height: 'auto' }}
-        alt='Codex Christi Main Logo'
-        src='/media/img/shop/shop-logo-1.svg'
-        onContextMenu={(event: MouseEvent<HTMLImageElement>) => {
-          event.preventDefault();
-        }}
-        onDragStart={(event: MouseEvent<HTMLImageElement>) => {
-          event.preventDefault();
-        }}
-      />
+      <picture>
+        <source srcSet='/media/img/shop/shop-logo-216.avif' type='image/avif' />
+        <source srcSet='/media/img/shop/shop-logo-216.webp' type='image/webp' />
+        <img
+          width={72}
+          height={48}
+          className='relative block pointer-events-auto !max-w-[unset]'
+          style={{ width: '72px', height: 'auto' }}
+          alt='Codex Christi Main Logo'
+          decoding='sync'
+          fetchPriority='high'
+          loading='eager'
+          src='/media/img/shop/shop-logo-1.svg'
+          onContextMenu={(event: MouseEvent<HTMLImageElement>) => {
+            event.preventDefault();
+          }}
+          onDragStart={(event: MouseEvent<HTMLImageElement>) => {
+            event.preventDefault();
+          }}
+        />
+      </picture>
     </CustomShopLink>
   );
 }
